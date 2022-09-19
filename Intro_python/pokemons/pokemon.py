9747562c119c7d5beaf409921c012418e8777e67
Jose Laruta <eduardo.laruta@gmail.com>
2022-09-19 09:12:26 -0400

    tarea1

---
+++ Intro_python/pokemons/pokemon.py
@@ -0,0 +1,26 @@
+# fuente: https://pokemondb.net/pokedex/all
+pokemons = [
+    {
+        "name": "bulbasaur",
+        "attack": 49,
+        "defense": 49
+    },
+    {
+        "name": "charmander",
+        "attack": 52,
+        "defense": 43
+    },    
+
+]
+
+def enfrentar_pokemon(pokemon_1, pokemon_2):
+    pass
+
+
+if __name__ == "__main__":
+    # generar todas las combinaciones de los enfrentamientos
+    # resultado final:
+    # bulbasaur vs charmander -> ganador: charmander
+    # 
+    # plus: guardar los resultados en un archivo de texto
+    pass
