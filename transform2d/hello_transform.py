2a61f6dd69d6c661bb4aac99a49f7a38c1a982ee
Jose Laruta <eduardo.laruta@gmail.com>
2022-09-26 08:46:56 -0400

    transformaciones plantilla

---
+++ transform2d/hello_transform.py
@@ -0,0 +1,66 @@
+from ast import main
+from turtle import mainloop
+import numpy as np
+from math import radians
+import pygame 
+from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT, KEYDOWN
+
+# configuraciones
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+
+class App:
+    def __init__(self, screen_width, screen_height, bg_color=(0, 0, 0)):
+        self.width = screen_width
+        self.height = screen_height
+        self.bg_color = bg_color
+        pygame.init()
+        self.screen = pygame.display.set_mode([self.width, self.height])
+        self.is_running = False
+        self.polygons = []
+    
+    def update(self):
+        self.screen.fill(self.bg_color)
+        for poly in self.polygons:
+            pygame.draw.polygon(self.screen, (0, 255, 0), poly, 3)
+
+        pygame.display.flip()
+
+    def add_polygon(self, polygon):
+        self.polygons.append(polygon)
+
+    def transform_poly(self, poly_idx, t_matrix):
+        pass
+
+    def run(self):
+        self.is_running = True
+
+        while self.is_running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.is_running = False
+                if event.type == KEYDOWN:
+                    if event.key == K_DOWN:
+                        self.polygons[0].translate(100, 100)
+                    if event.key == K_UP:
+                        self.polygons[0].translate(-100, -100)
+                    if event.key == K_LEFT:
+                        self.polygons[0].rotate(radians(20))
+                    
+
+            self.update()
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    app = App(SCREEN_WIDTH, SCREEN_HEIGHT)
+    app.add_polygon([
+            (5, 5), 
+            # (70, 5),
+            # (95, 45), 
+            (180, 45), 
+            (5, 80)
+    ])
+    
+    app.run()
